cfccb47c4a82e2157b99d8346c8d81ad42f70083
Luis Aroca Rosales <luis.aroca3@gmail.com>
2020-05-17 04:56:51 -0400

    Agregar archivos de proyecto.

---
+++ Mezclador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba.Net1
+{
+    class Mezclador : VehiculoComponentes
+    {
+        private string _tipo;
+
+        public Mezclador(string tipo)
+                        
+        {
+            _tipo = tipo;
+        }
+
+        public string Tipo { get => _tipo; set => _tipo = value; }
+    }
+}
---
+++ VehiculoComponentes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba.Net1
+{
+    abstract class VehiculoComponentes
+        {
+            private double _estado;
+            protected VehiculoComponentes()
+            {
+                _estado = 100.0;
+            }
+            public double Estado
+            {
+                get => _estado;
+                set => double.TryParse(value.ToString(), out _estado);
+            }
+        }
+    }
+
